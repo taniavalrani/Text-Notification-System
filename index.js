2ebf496fc2d9301e6ccb4a075f4c697b19a0468a
Ari Brown <Ari@Aris-MacBook-Pro-2.local>
2017-10-13 21:58:21 -0400

    update

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 const express = require('express')
 const app = express()
 
-app.use(express.bodyParser());
+app.use(express.static('/'));
 
 var TMClient = require('textmagic-rest-client');
 
 var c = new TMClient('aribrown', 'GuMIti0sFF4lgbFuiES3OAraqKRKhK');
 c.Messages.send({text: 'hey tania isn\'t it weird, i know your name', phones:'18577076323'}, function(err, res){
     console.log('Messages.send()', err, res);
+});
+
+app.get('/', function(request, response){
+	response.write("You should be practicing");
+	response.send();
 });
 
 app.post('/receive_message', function(request, response){
