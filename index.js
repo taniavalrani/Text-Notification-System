9a75cf2825ecc5692156072326e7c18ec8e128ac
Ari Brown <Ari@Aris-MacBook-Pro-2.local>
2017-10-13 23:54:28 -0400

    hi

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 
 const express = require('express');
 const app = express();
-var multiparty = require('multiparty');
+var twilio = require('twilio');
+
+var accountSid = 'AC5f860a6daa0ce6671bc6d6e03092d657'; 
+var authToken = '4e9930cf1af407f6e44a895d220b31cb';
+
+var client = require('twilio')(accountSid, authToken); 
 
 app.use(express.static('/'));
 
 var port = process.env.PORT || 8080;
 
-var TMClient = require('textmagic-rest-client');
+app.get('/send_message'function (req, res) {
+
+	client.messages.create({ 
+			to: "+18577076323", 
+			from: "+13179618398 ", 
+			body: "hi", 
+		}, 
+		function(err, message) { 
+			console.log(message.sid);
+	});
+});
+
+app.post('/sms', function(req, res) {
+  var twiml = new twilio.TwimlResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+
+
+});
 
 /* Send a message
 	var c = new TMClient('aribrown', 'GuMIti0sFF4lgbFuiES3OAraqKRKhK');
@@ -21,18 +45,5 @@ app.get('/', function(request, response){
 	response.send();
 });
 
-app.post('/receive_message', function(request, response){
-  	console.log(request.headers['content_type']);
-
-	// parse a file upload
-	var form = new multiparty.Form();
-
-	form.parse(request, function(err, fields, files) {
-		console.log(util.inspect({fields: fields, files: files}));
-	});
-
-
-	response.sendStatus(200);
-});
 
 app.listen(port);
